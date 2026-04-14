d61972f5d88bac0e95158bf45d67a459f8971b83
RDanMT <80799530+RDanMT@users.noreply.github.com>
2026-04-13 20:56:12 -0600

    Delete hospital directory

--- hospital/src/main/java/com/rdmt/hospital/repositories/PrescriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rdmt.hospital.repositories;
-
-import com.rdmt.hospital.models.Prescription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
-}
